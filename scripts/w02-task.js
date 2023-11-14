797c80b94082006509ea3c28355efda88bad2607
jonathanortega7OOO <jonathanortega7000@hotmail.com>
2023-11-14 13:25:20 -0400

    task2

---
+++ scripts/w02-task.js
@@ -0,0 +1,38 @@
+/* W02-Task - Profile Home Page */
+
+/* Step 1 - Setup type tasks - no code required */
+
+/* Step 2 - Variables */
+let fullName = "Jonathan Ortega Alvarez";
+let currentYear = "2023";
+let profilePicture = "images/profileimage.png";
+
+/* Step 3 - Element Variables */
+const nameElement = document.getElementById("name");
+const foodElement = document.getElementById("food");
+const yearElement = document.querySelector("#year");
+const imageElement = document.querySelector("img");
+
+
+/* Step 4 - Adding Content */
+
+nameElement.innerHTML = `<strong>${fullName}</strong>`;
+yearElement.textContent = currentYear;
+imageElement.setAttribute("src", profilePicture);
+imageElement.setAttribute("alt", `Profile image of ${fullName}`);
+
+
+/* Step 5 - Array */
+
+let favoriteFood = ["Pizza", "Barbecue", "Fried chicken"];
+foodElement.innerHTML = favoriteFood;
+
+let extraFood = "Fruit salad";
+favoriteFood.push(extraFood);
+foodElement.innerHTML += `<br>${favoriteFood}`;
+
+favoriteFood.shift();
+foodElement.innerHTML += `<br>${favoriteFood}`;
+
+favoriteFood.pop();
+foodElement.innerHTML += `<br>${favoriteFood}`;
